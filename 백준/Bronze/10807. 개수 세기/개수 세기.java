1bb9415d1fb269f252398b810fca3ec4c069d153
홍서은 <128138034+hseun@users.noreply.github.com>
2023-10-25 19:41:04 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,15 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] str = br.readLine().split(" ");
         int[] arr = new int[n];
         for(int i = 0; i < n; i++)
-            arr[i] = sc.nextInt();
-        int key = sc.nextInt();
+            arr[i] = Integer.parseInt(str[i]);
+        int key = Integer.parseInt(br.readLine());
         int count = 0;
-        for(int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < n; i++) {
             if(arr[i] == key)
                 count++;
         }
