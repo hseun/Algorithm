d940e383da935f0e3ce93a1a8ace197009633d7a
홍서은 <128138034+hseun@users.noreply.github.com>
2023-11-15 19:13:33 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	char name[21];
+	scanf("%s", name);
+	printf(":fan::fan::fan:\n");
+	printf(":fan::%s::fan:\n", name);
+	printf(":fan::fan::fan:\n");
+	return 0;
+}
