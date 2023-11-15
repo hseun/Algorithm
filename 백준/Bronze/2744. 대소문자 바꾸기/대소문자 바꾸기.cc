748ba230099e27441210e0cf292f65ca3f2ff814
홍서은 <128138034+hseun@users.noreply.github.com>
2023-11-15 10:30:27 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	char str[101];
+	scanf("%s", str);
+	int length = strlen(str);
+	for (int i = 0; i < length; i++) {
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		else if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+	printf("%s", str);
+	return 0;
+}
