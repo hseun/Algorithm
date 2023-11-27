a4c2484da8260df8c65a29ea60d3f4fa0755df95
홍서은 <128138034+hseun@users.noreply.github.com>
2023-11-27 20:05:36 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 128 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < n; i++) {
+            str = br.readLine();
+            StringBuffer sb = new StringBuffer(str);
+            System.out.println(sb.reverse());
+        }
+    }
+}
