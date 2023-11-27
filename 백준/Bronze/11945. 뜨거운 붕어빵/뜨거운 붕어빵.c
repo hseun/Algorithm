984a2689a32df880695be71dcf6e5f84c12f3506
홍서은 <128138034+hseun@users.noreply.github.com>
2023-11-27 20:03:16 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int n, m, arr[10][10];
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			scanf("%1d", &arr[i][j]);
+	for (int i = 0; i < n; i++) {
+		for (int j = m - 1; j >= 0; j--)
+			printf("%d", arr[i][j]);
+		printf("\n");
+	}
+	return 0;
+}
