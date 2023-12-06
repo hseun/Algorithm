09536fdffe3be0e07e9b3005f4c781ad1ab2bc9a
홍서은 <128138034+hseun@users.noreply.github.com>
2023-12-06 20:03:18 +0900

    [Bronze IV] Title: Rust Study, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/30033. Rust Study/Rust Study.c
+++ 백준/Bronze/30033. Rust Study/Rust Study.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-	int n, a[1000], b[1000], count = 0;
+	int n, a[1000], b, count = 0;
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++)
 		scanf("%d", &a[i]);
-	for (int i = 0; i < n; i++)
-		scanf("%d", &b[i]);
-	for (int i = 0; i < n; i++)
-		if (a[i] <= b[i]) count++;
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &b);
+		if (a[i] <= b) count++;
+	}
 	printf("%d", count);
 	return 0;
 }
