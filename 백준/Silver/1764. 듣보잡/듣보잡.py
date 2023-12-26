818c72fbf18a141ffa768239732acb2c862095ac
홍서은 <128138034+hseun@users.noreply.github.com>
2023-12-26 14:25:35 +0900

    [Silver IV] Title: 듣보잡, Time: 176 ms, Memory: 120240 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,12 +1,14 @@
-n, m = map(int, input().split())
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
 no_hear = set()
 for _ in range(n):
-    no_hear.add(input())
+    no_hear.add(sys.stdin.readline())
 no_see = set()
 for _ in range(m):
-    no_see.add(input())
+    no_see.add(sys.stdin.readline())
 no_hear_see = no_hear.intersection(no_see)
 no_hear_see = sorted(no_hear_see)
 print(len(no_hear_see))
 for name in no_hear_see:
-    print(name)
+    sys.stdout.write(name)
