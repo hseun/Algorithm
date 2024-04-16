b4fe0d5cded9b7f6a1cac3a39f0a1c1ffe081b73
홍서은 <128138034+hseun@users.noreply.github.com>
2024-04-16 12:23:20 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 4.19 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -1,9 +1,8 @@
 def solution(arr, queries):
-    answer = []
-    for q in queries:
-        array = [arr[i] for i in range(len(arr)) if q[0] <= i and i <= q[1] and arr[i] > q[2]]
-        if len(array) == 0:
-            answer.append(-1)
-        else:
-            answer.append(min(array))
+    answer = [-1] * len(queries)
+    for i in range(len(queries)):
+        s, e, k = queries[i][0], queries[i][1], queries[i][2]
+        temp = [n for n in arr[s:e+1] if n > k]
+        if len(temp) > 0:
+            answer[i] = min(temp)
     return answer
