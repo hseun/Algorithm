760945aecdf487e8efecdfdd2b92b31b92aae7c4
홍서은 <128138034+hseun@users.noreply.github.com>
2024-04-30 15:03:40 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,6 @@
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        a = sorted(array[command[0] - 1:command[1]])
+        answer.append(a[command[2] - 1])
+    return answer
