abc5a01692a4624af14fedec43609cbb85566342
홍서은 <128138034+hseun@users.noreply.github.com>
2025-03-04 19:20:37 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 840 ms, Memory: 106228 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,21 +1,27 @@
 import sys
 input = sys.stdin.readline
 
+def total_sum(arr):
+    sum_arr = [[0] * (n + 1) for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            sum_arr[i][j] = arr[i][j]
+            sum_arr[i][j] += sum_arr[i][j - 1] + sum_arr[i - 1][j]
+            sum_arr[i][j] -= sum_arr[i - 1][j - 1]
+    return sum_arr
+
+def section_sum(sum_arr, i1, j1, i2, j2):
+    result = sum_arr[i2][j2]
+    result -= sum_arr[i2][j1 - 1]
+    result -= sum_arr[i1 - 1][j2]
+    result += sum_arr[i1 - 1][j1 - 1]
+    return result
+
 n, m = map(int, input().split())
 table = [[0] * n]
 table += [[0] + list(map(int, input().split())) for _ in range(n)]
-sum_table = [[0] * (n + 1) for _ in range(n + 1)]
-
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        sum_table[i][j] = table[i][j]
-        sum_table[i][j] += sum_table[i][j - 1] + sum_table[i - 1][j]
-        sum_table[i][j] -= sum_table[i - 1][j - 1]
+sum_table = total_sum(table)
 
 for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
-    result = sum_table[x2][y2]
-    result -= sum_table[x2][y1 - 1]
-    result -= sum_table[x1 - 1][y2]
-    result += sum_table[x1 - 1][y1 - 1]
-    print(result)
+    print(section_sum(sum_table, x1, y1, x2, y2))
